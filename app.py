c71b0c41b7e0fb5ea13a8b1b672fc5f644396e85
ibrahem <ibra@gmail.ccom>
2025-07-20 22:47:46 +0400

    Add app.py file for Render deployment

---
+++ app.py
@@ -0,0 +1,15 @@
+"""
+WSGI app for student_housing project.
+"""
+
+import os
+import sys
+
+# Add the project directory to the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_housing.settings')
+
+application = get_wsgi_application()
--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Print Python path for debugging
+python -c "import sys; print(sys.path)"
+
 # Collect static files
 python manage.py collectstatic --no-input
 
