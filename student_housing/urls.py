b60df4294f7de5d3363f4981c42e2b92033f3d17
ibrahem <ibra@gmail.ccom>
2025-07-23 00:37:15 +0400

    Restore original home page

--- student_housing/urls.py
+++ student_housing/urls.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from apartments.views_debug import debug_home
+from apartments.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', debug_home, name='home'),  # Use debug home page
+    path('', home, name='home'),  # استخدام الصفحة الرئيسية الأصلية
     path('apartments/', include('apartments.urls')),
     path('users/', include('users.urls')),
     path('accounts/', include('allauth.urls')),
 ]
 
-# Add static and media URLs
+# إضافة مسارات الملفات الثابتة والوسائط
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
