3edff0dcf67b8f4ea7e3a70b9c7db8e964169744
ibrahem <ibra@gmail.ccom>
2025-07-23 20:22:20 +0400

    Fix Profile model fields and create simple profile templates

--- users/forms.py
+++ users/forms.py
@@ -20,4 +20,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image', 'phone', 'bio']
+        fields = ['user_type']  # استخدام الحقول الموجودة فقط
