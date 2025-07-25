7c3d88a990bb6ac641cd8a5d841def64897794aa
ibrahem <ibra@gmail.ccom>
2025-07-25 20:59:41 +0400

    Fix profile page display and AJAX functionality

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', login_pro, name='login'),
     path('logout/', logout_pro, name='logout'),
     path('register/', register_pro, name='register'),
-    path('profile/', views.profile_ajax, name='profile'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/ajax/', views.profile_ajax, name='profile_ajax'),
     path('profile/<str:username>/', views.view_profile, name='view_profile'),
 ]
